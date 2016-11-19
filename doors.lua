3c5868606733a70989b1cde461c3d49f98fea9b2
Tucker Bamberg <mootpoint23@gmail.com>
2016-11-19 09:23:25 -0500

    Added stone door, fixed steel door texture.

--- doors.lua
+++ doors.lua
@@ -7,7 +7,7 @@ minetest.register_node("travelnet:elevator_door_steel_open", {
 		description = "elevator door (open)",
 		drawtype = "nodebox",
                 -- top, bottom, side1, side2, inner, outer
-		tiles = {"default_stone.png"},
+		tiles = {"travelnet_steel_door.png"},
 		paramtype = "light",
 		paramtype2 = "facedir",
 		is_ground_content = true,
@@ -36,7 +36,7 @@ minetest.register_node("travelnet:elevator_door_steel_closed", {
 		description = "elevator door (closed)",
 		drawtype = "nodebox",
                 -- top, bottom, side1, side2, inner, outer
-		tiles = {"default_stone.png"},
+		tiles = {"travelnet_steel_door.png"},
 		paramtype = "light",
 		paramtype2 = "facedir",
 		is_ground_content = true,
@@ -60,6 +60,61 @@ minetest.register_node("travelnet:elevator_door_steel_closed", {
 })
 
 
+minetest.register_node("travelnet:elevator_door_stone_open", {
+		description = "elevator door (open)",
+		drawtype = "nodebox",
+                -- top, bottom, side1, side2, inner, outer
+		tiles = {"default_stone.png"},
+		paramtype = "light",
+		paramtype2 = "facedir",
+		is_ground_content = true,
+		groups = {snappy=2,choppy=2,oddly_breakable_by_hand=2,not_in_creative_inventory=1},
+                -- larger than one node but slightly smaller than a half node so that wallmounted torches pose no problem
+		node_box = {
+			type = "fixed",
+			fixed = {
+				{-0.90, -0.5,  0.4, -0.49, 1.5,  0.5},
+				{ 0.49, -0.5,  0.4,  0.9, 1.5,  0.5},
+			},
+		},
+		selection_box = {
+			type = "fixed",
+			fixed = {
+				{-0.9, -0.5,  0.4,  0.9, 1.5,  0.5},
+			},
+		},
+		drop = "travelnet:elevator_door_stone_closed",
+                on_rightclick = function(pos, node, puncher)
+                    minetest.add_node(pos, {name = "travelnet:elevator_door_stone_closed", param2 = node.param2})
+                end,
+})
+
+minetest.register_node("travelnet:elevator_door_stone_closed", {
+		description = "elevator door (closed)",
+		drawtype = "nodebox",
+                -- top, bottom, side1, side2, inner, outer
+		tiles = {"default_stone.png"},
+		paramtype = "light",
+		paramtype2 = "facedir",
+		is_ground_content = true,
+		groups = {snappy=2,choppy=2,oddly_breakable_by_hand=2},
+		node_box = {
+			type = "fixed",
+			fixed = {
+				{-0.5,  -0.5,  0.4, -0.01, 1.5,  0.5},
+				{ 0.01, -0.5,  0.4,  0.5,  1.5,  0.5},
+			},
+		},
+		selection_box = {
+			type = "fixed",
+			fixed = {
+				{-0.5, -0.5,  0.4,  0.5, 1.5,  0.5},
+			},
+		},
+                on_rightclick = function(pos, node, puncher)
+                    minetest.add_node(pos, {name = "travelnet:elevator_door_stone_open", param2 = node.param2})
+                end,
+})
 
 
 minetest.register_node("travelnet:elevator_door_glass_open", {
@@ -135,8 +190,16 @@ minetest.register_craft({
 			{"default:steel_ingot", "", "default:steel_ingot", }
 		        }
 	})
+
+minetest.register_craft({
+	        output = "travelnet:elevator_door_stone_closed",
+	        recipe = {
+		        {"default:stone_block", "", "default:stone_block", },
+			{"default:stone_block", "", "default:stone_block", },
+			{"default:stone_block", "", "default:stone_block", }
+		        }
+	})
 --      local old_node = minetest.get_node( pos );
 --      minetest.add_node(pos, {name = "travelnet:elevator_door_glass_closed", param2 = old_node.param2})
-
 
 
